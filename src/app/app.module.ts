dfc0c8afa4edfe2d868bb3340ca9230bf6d8fdaa
sharunkk1 <sharunkk1@gmail.com>
2019-06-02 12:29:17 +0530

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap/alert';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './template/navbar/navbar.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';  
 
+const routes: Routes = [  
+  {path: 'contact',component: ContactUsComponent},
+  {path: '',component: HomeComponent}
+  ];  
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContactUsComponent,
+    HomeComponent
   ],
   imports: [AlertModule.forRoot(),
-    BrowserModule
-  ],
+    BrowserModule,RouterModule.forRoot(routes)],  
+  exports: [RouterModule],  
   providers: [],
   bootstrap: [AppComponent]
 })
